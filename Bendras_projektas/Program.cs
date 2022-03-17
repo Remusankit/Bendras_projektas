b31755f33abc0c706b4f18f6fea9285358e74580
Remusankit <100715395+Remusankit@users.noreply.github.com>
2022-03-17 20:28:19 +0200

    Update Program.cs

--- Bendras_projektas/Program.cs
+++ Bendras_projektas/Program.cs
@@ -6,10 +6,21 @@ namespace Bendras_projektas
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             int spindulis = 6371;
             double plotas = 3.14 * Math.Pow(2, spindulis);
             Console.WriteLine(plotas);
             Console.WriteLine();
+=======
+            int spindulys = 6371;
+            double plotas = 3.14 * Math.Pow(2, spindulys);
+            Console.WriteLine($"Plotas"{plotas});
+
+            int spindulys2 = 6371;
+            double rutulioTuris = 1.3 * 3.14 * Math.Pow(3, spindulys2);
+            Console.WriteLine(rutulioTuris);
+            
+>>>>>>> Stashed changes
         }
     }
 }
